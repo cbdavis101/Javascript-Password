10fa57ac720b85b4fd7b194711d38c3b4f738e0c
Cam Davis <cbdavis101@gmail.com>
2022-01-15 15:48:52 -0600

    fixed syntax regarding randompassword

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 // Assignment code here
 
 // Creating Array for Length, Numbers, Lowercase, Uppercase, Special Characters
-let passwordLength = [];
+let passwordLength = 0;
 let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 let lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 let uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"] 
@@ -15,22 +15,25 @@ let generateBtn = document.querySelector("#generate");
 function writePassword() {
   // let password = generatePassword();
   let randomPassword = "";
-  console.log(passwordLength)
   for (var i = 0; i < passwordLength; i++) {
-   randomPassword += finalPass[Math.floor(Math.random() * finalPass.length)];  
-  }
-  console.log(randomPassword);
-
+   randomPassword += finalPass[randomNumber(0, finalPass.length)]
+  } 
+  
   let passwordText = document.querySelector("#password");
-  // passwordText.value = password (wrong syntax name)
+  passwordText.value = randomPassword;
+
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", generatePassword);
 
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}randomNumber(0, 2)
+
 // Creating Prompt Alerts for the Password Criteria 
 function generatePassword () {
-  let passwordLength = parseInt (prompt("Please enter the amount of characters you want for your password. Please choose more than 8 but less than 128 characters total!"));
+  passwordLength = parseInt (prompt("Please enter the amount of characters you want for your password. Please choose more than 8 but less than 128 characters total!"));
     // Creating IF Loops to ensure the value entered meets the criteria 
       if (isNaN(passwordLength)) {
 
@@ -79,6 +82,3 @@ function generatePassword () {
 
   writePassword();
 }
-
-
-
